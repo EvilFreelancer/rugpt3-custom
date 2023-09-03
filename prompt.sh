a789af77dc1d41767efbad1297eeda18bab7772c
Paul Rock <paul@drteam.rocks>
2023-09-03 04:23:47 +0300

    prompt.sh fixed

--- prompt.sh
+++ prompt.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-[ ! -f generate_transformers.py ] && wget https://raw.githubusercontent.com/EvilFreelancer/ru-gpts/master/generate_transformers.py
+[ ! -f generate_transformers.py ] && \
+  wget https://raw.githubusercontent.com/EvilFreelancer/ru-gpts/master/generate_transformers.py
 
-model='ai-forever/ruGPT-3.5-13B'
+#model='ai-forever/ruGPT-3.5-13B'
+#python3 generate_transformers.py \
+#  --model_type=gpt2 \
+#  --model_name_or_path=${model} \
+#  --repetition_penalty=1.1 \
+#  --k=40 \
+#  --p=0.85 \
+#  --length=256 \
+#  --stop_token='<|endoftext|>' \
+#  --load_in_8bit
+
+model="./dostoevsky_doesnt_write_it"
 python3 generate_transformers.py \
   --model_type=gpt2 \
   --model_name_or_path=${model} \
   --repetition_penalty=1.2 \
   --k=5 \
   --p=0.95 \
-  --length=100 \
-  --stop_token='<|endoftext|>' \
-  --load_in_8bit
-
-#model="./dostoevsky_doesnt_write_it"
-#python3 generate_transformers.py \
-#  --model_type=gpt2 \
-#  --model_name_or_path=${model} \
-#  --repetition_penalty=1.2 \
-#  --k=5 \
-#  --p=0.95 \
-#  --length=100
+  --length=100
